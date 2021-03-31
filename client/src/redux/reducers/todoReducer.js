5b83f7d8e3fd8dfe29de5e0f5f63c737e3dbb29a
Triumph Odemudia <triumphodemudia@192.168.1.159>
2021-03-31 21:22:15 +0100

    Added a couple of images for readme

--- client/src/redux/reducers/todoReducer.js
+++ client/src/redux/reducers/todoReducer.js
@@ -4,37 +4,27 @@ import {
   ADD_TODO_SUCCESS,
   ADD_TODO_FAIL,
   DELETE_TODO_SUCCESS,
-  DELETE_TODO_FAIL
+  DELETE_TODO_FAIL,
 } from "../actions/types.js";
 
 const initialState = {
-  todos: [
-    {_id: '1',
-      todoString: "Testing",
-      isChecked: false},
-      {_id: '2',
-      todoString: "I love Testing",
-      isChecked: false},
-      {_id: '3',
-      todoString: "Fuck Testing",
-      isChecked: false},
-  ],
+  todos: [{ _id: "2", todoString: "I love Testing", isChecked: false }],
 };
 
 export default function (state = initialState, action) {
   switch (action.type) {
     case GET_TODOS_SUCCESS:
-      return { todos: [...state.todos, ...action.payload] }
+      return { todos: [...state.todos, ...action.payload] };
     case ADD_TODO_SUCCESS:
-      return [...state.todos, action.payload]
+      return [...state.todos, action.payload];
     case DELETE_TODO_SUCCESS:
-      return [...state.todos, action.payload]
-      case GET_TODOS_FAIL:
-      case ADD_TODO_FAIL:
-      case DELETE_TODO_FAIL:
-        return {
-          ...state,
-        }
+      return [...state.todos, action.payload];
+    case GET_TODOS_FAIL:
+    case ADD_TODO_FAIL:
+    case DELETE_TODO_FAIL:
+      return {
+        ...state,
+      };
     default:
       return state;
   }
